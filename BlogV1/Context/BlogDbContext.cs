46502b4fef93fe0efc7957b91494234539062d52
Win10 <Win10@DESKTOP-CRRT34M>
2025-03-28 22:31:12 +0300

    Add project files.

---
+++ BlogV1/Context/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogV1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogV1.Context
+{
+    public class BlogDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=Desktop-CRRT34M\\SQLEXPRESS;Database=BlogV1;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
